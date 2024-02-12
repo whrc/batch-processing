aa39b654b0e05ab88ca6d5167121673a83af02f9
Doğukan Teber <dogukanteber1@hotmail.com>
2024-02-12 11:07:09 +0300

    Refactor base.py and write a comment

--- src/batch_processing/cmd/base.py
+++ src/batch_processing/cmd/base.py
@@ -4,18 +4,28 @@ from abc import ABC, abstractmethod
 
 class BaseCommand(ABC):
     def __init__(self):
-        self.home_dir = os.getenv("HOME")
         self.user = os.getenv("USER")
-        self.dvmdostem_bin_path = f"{self.home_dir}/dvm-dos-tem/dvmdostem"
-        self.output_spec_path = f"{self.home_dir}/dvm-dos-tem/config/output_spec.csv"
-        self.parameters_path = f"{self.home_dir}/dvm-dos-tem/parameters/"
+        self.home_dir = os.getenv("HOME")
+
+        self.dvmdostem_path = f"{self.home_dir}/dvm-dos-tem"
+        self.dvmdostem_bin_path = f"{self.dvmdostem_path}/dvmdostem"
+
+        # You might notice that the only variable which has a trailing
+        # slash is in the below one. config.js file has it this way.
+        # Therefore, I didn't remove it. The trailing slash can be
+        # removed after making sure that `dvmdostem` is working
+        # without a problem.
+        self.parameters_path = f"{self.dvmdostem_path}/parameters/"
+        self.config_path = f"{self.dvmdostem_path}/config/config.js"
+        self.output_spec_path = f"{self.dvmdostem_path}/config/output_spec.csv"
+
         self.exacloud_user_dir = f"/mnt/exacloud/{self.user}"
-        self.config_path = f"{self.home_dir}/dvm-dos-tem/config/config.js"
-        self.input_dir = f"/mnt/exacloud/{self.user}/input"
-        self.output_dir = f"/mnt/exacloud/{self.user}/output"
+        self.input_dir = f"{self.exacloud_user_dir}/input"
+        self.output_dir = f"{self.exacloud_user_dir}/output"
         self.slurm_log_dir = f"{self.exacloud_user_dir}/slurm-logs"
-        self.batch_dir = f"{self.output_dir}/batch-run"
         self.result_dir = f"{self.exacloud_user_dir}/all-merged"
+
+        self.batch_dir = f"{self.output_dir}/batch-run"
 
     @abstractmethod
     def execute(self):
