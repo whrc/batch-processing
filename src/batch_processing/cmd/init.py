16368725e8026b8ee05a81e3ec57093f18824f64
Doğukan Teber <dogukanteber1@hotmail.com>
2024-04-16 15:08:23 +0300

    Pull the compiled dvm-dos-tem from the bucket

--- src/batch_processing/cmd/init.py
+++ src/batch_processing/cmd/init.py
@@ -1,10 +1,9 @@
-import subprocess
 from pathlib import Path
 
 from rich import print
 
 from batch_processing.cmd.base import BaseCommand
-from batch_processing.utils.utils import download_file, run_command
+from batch_processing.utils.utils import download_directory, download_file, run_command
 
 
 class InitCommand(BaseCommand):
@@ -17,28 +16,30 @@ class InitCommand(BaseCommand):
             raise ValueError("Do not run as root or with sudo.")
 
         # Copy necessary files from the cloud
+        # dvm-dos-tem version v0.7.0 - 2023-06-14
+        # Note: dvmdostem binary is compiled with USEMPI=true flag
         print("[bold blue]Copying dvm-dos-tem to the home directory...[/bold blue]")
-        # download_directory("gcp-slurm", "dvm-dos-tem/", self.home_dir)
-        subprocess.run(
-            f"git clone https://github.com/uaf-arctic-eco-modeling/dvm-dos-tem.git {self.home_dir}/dvm-dos-tem",
-            shell=True,
-            check=True,
-            executable="/bin/bash",
-        )
+        download_directory("gcp-slurm", "dvm-dos-tem/", self.home_dir)
+        # subprocess.run(
+        #     f"git clone https://github.com/uaf-arctic-eco-modeling/dvm-dos-tem.git {self.home_dir}/dvm-dos-tem",
+        #     shell=True,
+        #     check=True,
+        #     executable="/bin/bash",
+        # )
         print(f"[bold green]dvm-dos-tem is copied to {self.home_dir}[/bold green]")
 
-        print("[bold blue]Compile dvmdostem binary...[/bold blue]")
-        command = f"""
-        cd {self.home_dir}/dvm-dos-tem && \
-        export DOWNLOADPATH=/dependencies && \
-        . $DOWNLOADPATH/setup-env.sh && \
-        module load openmpi && \
-        make USEMPI=true
-        """
+        # print("[bold blue]Compile dvmdostem binary...[/bold blue]")
+        # command = f"""
+        # cd {self.home_dir}/dvm-dos-tem && \
+        # export DOWNLOADPATH=/dependencies && \
+        # . $DOWNLOADPATH/setup-env.sh && \
+        # module load openmpi && \
+        # make USEMPI=true
+        # """
 
-        subprocess.run(command, shell=True, check=True, executable="/bin/bash")
-        # run_command(["chmod", "+x", self.dvmdostem_bin_path])
-        print("[bold green]dvmdostem binary is successfully compiled.[/bold green]")
+        # subprocess.run(command, shell=True, check=True, executable="/bin/bash")
+        # print("[bold green]dvmdostem binary is successfully compiled.[/bold green]")
+        run_command(["chmod", "+x", self.dvmdostem_bin_path])
 
         download_file(
             "gcp-slurm",
@@ -72,7 +73,9 @@ class InitCommand(BaseCommand):
 
         run_command(["lfs", "setstripe", "-S", "0.25M", self.exacloud_user_dir])
 
-        print("\n[bold green]The initialization is successfully completed.[/bold green]")
+        print(
+            "\n[bold green]The initialization is successfully completed.[/bold green]"
+        )
         print(
             f"[bold green]Check {self.home_dir} and {self.exacloud_user_dir} for the changes.[/bold green]\n"
         )
