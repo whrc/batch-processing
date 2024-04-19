19a08485493b8c80f61dcea851f5a63f74fd518a
Doğukan Teber <dogukanteber1@hotmail.com>
2024-04-19 16:31:18 +0300

    Fix the file extension for the log file

--- src/batch_processing/cmd/run_check.py
+++ src/batch_processing/cmd/run_check.py
@@ -25,7 +25,7 @@ class RunCheckCommand(BaseCommand):
         super().__init__()
         logging.basicConfig(
             level=logging.NOTSET,
-            filename=f"{os.path.join(self.exacloud_user_dir, "run_check.py")}",
+            filename=f"{os.path.join(self.exacloud_user_dir, "run_check.out")}",
         )
 
         self._args = args
