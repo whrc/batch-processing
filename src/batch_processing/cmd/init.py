36301e2b4fc9951c622d63bb7ee60adb9606e7b2
Dogukan Teber <dogukanteber1@hotmail.com>
2024-07-10 16:49:05 +0000

    Refactor init.py

--- src/batch_processing/cmd/init.py
+++ src/batch_processing/cmd/init.py
@@ -1,11 +1,6 @@
-from pathlib import Path
-
-from rich import print
-
 from batch_processing.cmd.base import BaseCommand
 from batch_processing.utils.utils import download_directory, download_file, run_command
 
-# todo: create output dir
 
 class InitCommand(BaseCommand):
     def __init__(self, args):
@@ -16,42 +11,23 @@ class InitCommand(BaseCommand):
         if self.user == "root":
             raise ValueError("Do not run as root or with sudo.")
 
+        self.exacloud_user_dir.mkdir(exist_ok=True)
+        self.slurm_log_dir.mkdir(exist_ok=True)
+        self.output_dir.mkdir(exist_ok=True)
+
         # Copy necessary files from the cloud
         # dvm-dos-tem version v0.7.0 - 2023-06-14
         # Note: dvmdostem binary is compiled with USEMPI=true flag
-        print("[bold blue]Copying dvm-dos-tem to the home directory...[/bold blue]")
+        print("Copying dvm-dos-tem to the home directory...")
+
         download_directory("gcp-slurm", "dvm-dos-tem/", self.home_dir)
-        # subprocess.run(
-        #     f"git clone https://github.com/uaf-arctic-eco-modeling/dvm-dos-tem.git {self.home_dir}/dvm-dos-tem",
-        #     shell=True,
-        #     check=True,
-        #     executable="/bin/bash",
-        # )
-        print(f"[bold green]dvm-dos-tem is copied to {self.home_dir}[/bold green]")
-
-        # print("[bold blue]Compile dvmdostem binary...[/bold blue]")
-        # command = f"""
-        # cd {self.home_dir}/dvm-dos-tem && \
-        # export DOWNLOADPATH=/dependencies && \
-        # . $DOWNLOADPATH/setup-env.sh && \
-        # module load openmpi && \
-        # make USEMPI=true
-        # """
-
-        # subprocess.run(command, shell=True, check=True, executable="/bin/bash")
-        # print("[bold green]dvmdostem binary is successfully compiled.[/bold green]")
-        run_command(["chmod", "+x", self.dvmdostem_bin_path])
-
         download_file(
             "gcp-slurm",
             "output_spec.csv",
             self.output_spec_path,
         )
-        print(
-            f"[bold blue]output_spec.csv is copied to {self.output_spec_path}[/bold blue]"
-        )
 
-        run_command(["sudo", "-H", "mkdir", "-p", self.exacloud_user_dir])
+        run_command(["chmod", "+x", self.dvmdostem_bin_path])
         run_command(
             [
                 "sudo",
@@ -62,23 +38,7 @@ class InitCommand(BaseCommand):
                 self.exacloud_user_dir,
             ]
         )
-        print(
-            "[bold green]A new directory is created for the current user, "
-            f"{self.exacloud_user_dir}[/bold green]"
-        )
-
-        self.slurm_log_dir.mkdir(exist_ok=True)
-        self.output_dir.mkdir(exist_ok=True)
-
-        print(
-            f"[bold blue]slurm-logs directory is created under {self.exacloud_user_dir}[/bold blue]"
-        )
-
         run_command(["lfs", "setstripe", "-S", "0.25M", self.exacloud_user_dir])
 
-        print(
-            "\n[bold green]The initialization is successfully completed.[/bold green]"
-        )
-        print(
-            f"[bold green]Check {self.home_dir} and {self.exacloud_user_dir} for the changes.[/bold green]\n"
-        )
+        print("The initialization is successfully completed.")
+        print(f"Check {self.home_dir} and {self.exacloud_user_dir} for the changes.")
