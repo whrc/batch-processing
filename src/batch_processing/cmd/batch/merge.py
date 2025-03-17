b3bda64a4e3ac35745ac37b5067b19ba38d145a1
Dogukan Teber <dogukanteber1@hotmail.com>
2025-03-17 20:38:33 +0000

    Replace NCO-based merging with xarray-based one

--- src/batch_processing/cmd/batch/merge.py
+++ src/batch_processing/cmd/batch/merge.py
@@ -1,9 +1,6 @@
-import shutil
-import subprocess
 import gcsfs
 import xarray as xr
 import glob
-from collections import defaultdict
 from pathlib import Path
 from dask.distributed import Client
 
@@ -19,104 +16,14 @@ class BatchMergeCommand(BaseCommand):
         self._args = args
         self.base_batch_dir = Path(self.exacloud_user_dir, args.batches)
         self.result_dir = self.base_batch_dir / "all_merged"
+        self.result_dir.mkdir(parents=True, exist_ok=True)
 
-    def _merge_with_nco(self):
-        batch_output_dirs = self.base_batch_dir.glob("*/output")
-
-        # keys hold the file names
-        # values hold the paths of that files
-        grouped_files = defaultdict(list)
-
-        for batch_dir in batch_output_dirs:
-            for file in batch_dir.iterdir():
-                grouped_files[file.name].append(file)
-
-        print("sorting the files")
-        for file_name in grouped_files:
-            grouped_files[file_name].sort(key=get_batch_number)
-
-        print("renaming and copying the files")
-        for file_name in grouped_files:
-            all_files = grouped_files[file_name]
-            new_files = []
-            for index, file in enumerate(all_files):
-                temp_file = Path(file.parent / f"{file.stem}_{index}.nc")
-                file_as_str = file.as_posix()
-                if any(map(file_as_str.__contains__, ["restart", "run_status"])):
-                    shutil.copy(file, temp_file)
-                else:
-                    subprocess.run(
-                        [
-                            "ncrename",
-                            "-O",
-                            "-h",
-                            "-d",
-                            "x,X",
-                            "-d",
-                            "y,Y",
-                            file_as_str,
-                            temp_file,
-                        ]
-                    )
-
-                new_files.append(temp_file)
-
-            grouped_files[file_name] = new_files
-
-        for file_name in grouped_files:
-            all_files = grouped_files[file_name]
-            for file in all_files:
-                subprocess.run(
-                    [
-                        "ncks",
-                        "-O",
-                        "-h",
-                        "--mk_rec_dmn",
-                        "Y",
-                        file,
-                        file,
-                    ]
-                )
-
-        print("ncap2")
-        for file_name in grouped_files:
-            all_files = grouped_files[file_name]
-            for index, file in enumerate(all_files):
-                try:
-                    _ = subprocess.run(
-                        [
-                            "ncap2",
-                            "-O",
-                            "-h",
-                            "-s",
-                            f"Y[$Y]={index}; X[$X]=array(0, 1, $X);",
-                            file.as_posix(),
-                            file.as_posix(),
-                        ],
-                        check=True,
-                        capture_output=True,
-                        text=True,
-                    )
-                except subprocess.CalledProcessError as e:
-                    print(f"Error occurred: {e}")
-                    print(f"Output: {e.stdout}")
-                    print(f"Error output: {e.stderr}")
-
-        self.result_dir.mkdir(exist_ok=True)
-        print("concatenating the files")
-        for file_name in grouped_files:
-            all_files = grouped_files[file_name]
-            subprocess.run(
-                [
-                    "ncrcat",
-                    "-O",
-                    "-h",
-                    *all_files,
-                    self.result_dir / file_name,
-                ]
-            )
-            # remove the intermediary files
-            _ = [file.unlink() for file in all_files]
+    def _merge_small_dataset(self, output_file, output_path):
+        path = self.base_batch_dir / "batch_*" / "output" / output_file
+        files = sorted(glob.glob(path.as_posix()), key=get_batch_number)
+        print(f"Reading {output_file}")
+        ds = xr.open_mfdataset(files, engine="h5netcdf", combine="nested", concat_dim="y", data_vars="minimal", coords="minimal", compat="override", decode_cf=False, decode_times=False)
+        ds.to_netcdf(f"{output_path}/{output_file}")
 
     def _merge(self, output_file, bucket_path):
         fs = get_gcsfs()
@@ -155,13 +62,17 @@ class BatchMergeCommand(BaseCommand):
         print(f"File path: {file_path}")
         x, y = get_dimensions(file_path)
         total_batch_count = len([p for p in self.base_batch_dir.iterdir() if "batch_" in p.as_posix()])
+        print("Total batch count is ", total_batch_count)
 
         assert y == 1
         y *= total_batch_count
         total_cell_count = x * y
 
         if total_cell_count < self.__MIN_CELL_COUNT_FOR_DASK:
-            self._merge_with_nco()
+            path = self.base_batch_dir / "batch_0" / "output"
+            output_files = [f.name for f in path.iterdir()]
+            for f in output_files:
+                self._merge_small_dataset(f, self.result_dir)
         else:
             if self._args.bucket_path == "":
                 raise ValueError(
