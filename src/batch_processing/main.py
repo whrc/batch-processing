2e703139ff06582d9a1e222e7f4dc30ca4a84c07
Dogukan Teber <dogukanteber1@hotmail.com>
2025-03-17 21:07:41 +0000

    Replace argparse with typer package

--- src/batch_processing/main.py
+++ src/batch_processing/main.py
@@ -1,4 +1,6 @@
-import argparse
+import typer
+from typing import Optional
+from enum import Enum
 import textwrap
 
 import lazy_import
@@ -26,248 +28,302 @@ SliceInputCommand = lazy_import.lazy_class(
 )
 
 
-def add_common_dvmdostem_arguments(parser: argparse.ArgumentParser) -> None:
-    parser.add_argument(
-        "-p", type=int, default=0, help="Number of PRE RUN years to run. By default, 0"
+class LogLevel(str, Enum):
+    debug = "debug"
+    info = "info"
+    note = "note"
+    warn = "warn"
+    err = "err"
+    fatal = "fatal"
+    disabled = "disabled"
+
+
+class SlurmPartition(str, Enum):
+    spot = "spot"
+    compute = "compute"
+
+
+app = typer.Typer(
+    help=textwrap.dedent(
+        """
+        bp (or batch-processing) is a specialized internal tool designed for
+        scientists at the Woodwell Climate Research Center.
+
+        Optimized for execution in the GCP (Google Cloud Platform) cluster,
+        this tool streamlines the process of setting up and managing Slurm-based
+        computational environments. It simplifies tasks such as configuring run
+        parameters, partitioning input data into manageable batches, and executing
+        these batches efficiently.
+
+        Its primary aim is to enhance productivity and reduce manual setup
+        overhead in complex data processing workflows, specifically tailored
+        to the needs of climate research and analysis."""
     )
-    parser.add_argument(
-        "-e",
-        type=int,
-        default=0,
-        help="Number of EQUILIBRIUM years to run. By default, 0",
+)
+
+batch_app = typer.Typer(help="Batch related operations")
+app.add_typer(batch_app, name="batch")
+
+
+@app.callback()
+def callback(
+    version: Optional[bool] = typer.Option(
+        None, "--version", "-v", help="Show the version and exit.", is_flag=True
     )
-    parser.add_argument(
-        "-s", type=int, default=0, help="Number of SPINUP years to run. By default, 0"
-    )
-    parser.add_argument(
-        "-t",
-        type=int,
-        default=0,
-        help="Number of TRANSIENT years to run. By default, 0",
-    )
-    parser.add_argument(
-        "-n", type=int, default=0, help="Number of SCENARIO years to run. By default, 0"
-    )
-    parser.add_argument(
+):
+    if version:
+        typer.echo("bp 1.1.0")
+        raise typer.Exit()
+
+
+@app.command("init")
+def init():
+    """Initialize the environment for running the simulation."""
+    args = type("Args", (), {})()
+    InitCommand(args).execute()
+
+
+@batch_app.command("postprocess")
+def batch_postprocess(
+    light: bool = typer.Option(
+        False, "--light", help="Perform light post-processing"
+    ),
+    heavy: bool = typer.Option(
+        False, "--heavy", help="Perform heavy post-processing"
+    ),
+):
+    """Post-process the merged files and creates pre-define graphs."""
+    if not light and not heavy:
+        typer.echo("Error: Either --light or --heavy must be specified")
+        raise typer.Exit(1)
+    
+    args = type("Args", (), {"light": light, "heavy": heavy})()
+    BatchPostprocessCommand(args).execute()
+
+
+@app.command("diff")
+def diff(
+    path_one: str = typer.Argument(
+        ..., help="First path to compare"
+    ),
+    path_two: str = typer.Argument(
+        ..., help="Second path to compare"
+    ),
+):
+    """
+    Compare the NetCDF files in the given directories.
+    The given two directories must contain the same files.
+    """
+    args = type("Args", (), {"path_one": path_one, "path_two": path_two})()
+    DiffCommand(args).execute()
+
+
+# This duplicated definition is removed since we've replaced it with a more complete one below
+
+
+# This duplicated definition is removed since we've replaced it with a more complete one below
+
+
+@app.command("slice_input")
+def slice_input(
+    input_path: str = typer.Option(
+        ..., "--input-path", "-i", help="Path to the input folder to slice"
+    ),
+    output_path: str = typer.Option(
+        ..., "--output-path", "-o", help="Path for writing the sliced input dataset"
+    ),
+    force: bool = typer.Option(
+        False, "--force", "-f", help="Override if the given output path exists"
+    ),
+    launch_as_job: bool = typer.Option(
+        False,
+        "--launch-as-job",
         "-l",
-        "--log-level",
-        choices=["debug", "info", "note", "warn", "err", "fatal", "disabled"],
-        default="disabled",
-        help="Set the log level",
-    )
+        help="Never pass this flag. It will be used internally to launch this command as a separate job.",
+    ),
+):
+    """
+    Slices the given input data into 10 smaller folders.
+    To use this command, the given input has to have at least 500,000 cells.
+    """
+    args = type(
+        "Args",
+        (),
+        {
+            "input_path": input_path,
+            "output_path": output_path,
+            "force": force,
+            "launch_as_job": launch_as_job,
+        },
+    )()
+    SliceInputCommand(args).execute()
 
 
-def add_batch_path_argument(parser: argparse.ArgumentParser) -> None:
-    parser.add_argument(
-        "-b",
+# Update the command definitions to include the common parameters directly
+# This is a better approach than trying to add parameters after command definition
+
+# For batch split command
+@batch_app.command("split")
+def batch_split(
+    slurm_partition: SlurmPartition = typer.Option(
+        SlurmPartition.spot,
+        "--slurm-partition",
+        "-sp",
+        help="Specificy the Slurm partition.",
+    ),
+    input_path: str = typer.Option(
+        ...,
+        "--input-path",
+        "-i",
+        help=(
+            "Remote or local path to the directory that contains the input files. "
+            "If remote, prefix the path with 'gcs:// '. "
+            "Remote path example: gcs://my-bucket/my-site. "
+            "Local path example: /mnt/exacloud/dvmdostem-inputs/cru-ts40_ar5_rcp85_ncar-ccsm4_Toolik_50x50"
+        ),
+    ),
+    launch_as_job: bool = typer.Option(
+        False,
+        "--launch-as-job",
+        help="Never pass this flag. It will be used internally to launch this command as a separate job.",
+    ),
+    batches: str = typer.Option(
+        ...,
         "--batches",
-        required=True,
+        "-b",
         help=(
             "Path to store the splitted batches. The given path will be concataned "
-            "with /mnt/exacloud/$USER",
+            "with /mnt/exacloud/$USER"
         ),
-    )
+    ),
+    p: int = typer.Option(0, help="Number of PRE RUN years to run. By default, 0"),
+    e: int = typer.Option(0, help="Number of EQUILIBRIUM years to run. By default, 0"),
+    s: int = typer.Option(0, help="Number of SPINUP years to run. By default, 0"),
+    t: int = typer.Option(0, help="Number of TRANSIENT years to run. By default, 0"),
+    n: int = typer.Option(0, help="Number of SCENARIO years to run. By default, 0"),
+    log_level: LogLevel = typer.Option(
+        LogLevel.disabled, "--log-level", "-l", help="Set the log level"
+    ),
+):
+    """Split the given input data into smaller batches."""
+    # Create args object for compatibility with command class
+    all_args = {
+        "slurm_partition": slurm_partition.value,
+        "input_path": input_path,
+        "launch_as_job": launch_as_job,
+        "batches": batches,
+        "p": p,
+        "e": e,
+        "s": s,
+        "t": t,
+        "n": n,
+        "log_level": log_level.value,
+    }
+    args = type("Args", (), all_args)()
+    BatchSplitCommand(args).execute()
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="bp",
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=textwrap.dedent(
-            """
-            bp (or batch-processing) is a specialized internal tool designed for
-            scientists at the Woodwell Climate Research Center.
-
-            Optimized for execution in the GCP (Google Cloud Platform) cluster,
-            this tool streamlines the process of setting up and managing Slurm-based
-            computational environments. It simplifies tasks such as configuring run
-            parameters, partitioning input data into manageable batches, and executing
-            these batches efficiently.
-
-            Its primary aim is to enhance productivity and reduce manual setup
-            overhead in complex data processing workflows, specifically tailored
-            to the needs of climate research and analysis."""
-        ),
-        epilog="Use bp <command> --help for detailed help.",
-        add_help=False,
-    )
-    parser.add_argument(
-        "-h", "--help", action="store_true", help="Show this help message and exit"
-    )
-    parser.add_argument(
-        "-v", "--version", action="version", version="%(prog)s 1.1.0"
-    )
-
-    subparsers = parser.add_subparsers(title="Available commands", metavar="")
-
-    parser_init = subparsers.add_parser(
-        "init", help="Initialize the environment for running the simulation"
-    )
-    parser_init.set_defaults(func=lambda args: InitCommand(args).execute())
-
-    parser_batch = subparsers.add_parser(
-        "batch",
-        help="Batch related operations",
-    )
-    batch_subparsers = parser_batch.add_subparsers(
-        title="Available subcommands", metavar=""
-    )
-
-    parser_batch_split = batch_subparsers.add_parser(
-        "split", help="Split the given input data into smaller batches"
-    )
-
-    parser_batch_split.add_argument(
-        "-sp",
-        "--slurm-partition",
-        choices=["spot", "compute"],
-        default="spot",
-        help="Specificy the Slurm partition. By default, spot",
-    )
-    parser_batch_split.add_argument(
-        "-i",
-        "--input-path",
-        required=True,
+@batch_app.command("run")
+def batch_run(
+    batches: str = typer.Option(
+        ...,
+        "--batches",
+        "-b",
         help=(
-            "Remote or local path to the directory that contains the input files. ",
-            "If remote, prefix the path with 'gcs:// '"
-            "Remote path example: ",
-            "gcs://my-bucket/my-site",
-            "\n",
-            "Local path example: ",
-            "/mnt/exacloud/dvmdostem-inputs/cru-ts40_ar5_rcp85_ncar-ccsm4_Toolik_50x50",
+            "Path to store the splitted batches. The given path will be concataned "
+            "with /mnt/exacloud/$USER"
         ),
-    )
-    parser_batch_split.add_argument(
-        "--launch-as-job",
-        action="store_true",
-        help="Never pass this flag. It will be used internally "
-        "to lauch this command as a separate job.",
-    )
+    ),
+):
+    """Submit the batches to the Slurm queue."""
+    args = type("Args", (), {"batches": batches})()
+    BatchRunCommand(args).execute()
 
-    add_common_dvmdostem_arguments(parser_batch_split)
-    add_batch_path_argument(parser_batch_split)
 
-    parser_batch_split.set_defaults(func=lambda args: BatchSplitCommand(args).execute())
-
-    parser_batch_run = batch_subparsers.add_parser(
-        "run", help="Submit the batches to the Slurm queue"
-    )
-
-    add_batch_path_argument(parser_batch_run)
-
-    parser_batch_run.set_defaults(func=lambda args: BatchRunCommand(args).execute())
-
-    parser_batch_merge = batch_subparsers.add_parser("merge", help="Merge the batches")
-
-    parser_batch_merge.add_argument(
+@batch_app.command("merge")
+def batch_merge(
+    batches: str = typer.Option(
+        ...,
+        "--batches",
+        "-b",
+        help=(
+            "Path to store the splitted batches. The given path will be concataned "
+            "with /mnt/exacloud/$USER"
+        ),
+    ),
+    bucket_path: Optional[str] = typer.Option(
+        None,
         "--bucket-path",
         help=(
             "Bucket path to write the results into. "
             "Required when the total cell size is greater than 40,000."
-        )
-    )
+        ),
+    ),
+):
+    """Merge the batches."""
+    args = type("Args", (), {"batches": batches, "bucket_path": bucket_path})()
+    BatchMergeCommand(args).execute()
 
-    add_batch_path_argument(parser_batch_merge)
 
-    parser_batch_merge.set_defaults(func=lambda args: BatchMergeCommand(args).execute())
-
-    parser_batch_postprocess = batch_subparsers.add_parser(
-        "postprocess",
-        help="Post-process the merged files and creates pre-define graphs",
-    )
-
-    batch_postprocess_group = parser_batch_postprocess.add_mutually_exclusive_group(
-        required=True
-    )
-    batch_postprocess_group.add_argument(
-        "--light", action="store_true", help="Perform light post-processing"
-    )
-    batch_postprocess_group.add_argument(
-        "--heavy", action="store_true", help="Perform heavy post-processing"
-    )
-
-    parser_batch_postprocess.set_defaults(
-        func=lambda args: BatchPostprocessCommand(args).execute()
-    )
-
-    parser_diff = subparsers.add_parser(
-        "diff",
-        help="Compare the NetCDF files in the given directories. "
-        "The given two directories must contain the same files.",
-    )
-
-    parser_diff.add_argument("path_one")
-    parser_diff.add_argument("path_two")
-    parser_diff.set_defaults(func=lambda args: DiffCommand(args).execute())
-
-    parser_extract = subparsers.add_parser(
-        "extract_cell", help="Extracts a single cell and creates a batch"
-    )
-    parser_extract.add_argument(
-        "-i", "--input-path", required=True, help="Path to the input folder"
-    )
-    parser_extract.add_argument(
-        "-o", "--output-path", required=True, help="Path to the output folder"
-    )
-    parser_extract.add_argument(
-        "-X", type=int, required=True, help="The row to extract"
-    )
-    parser_extract.add_argument(
-        "-Y", type=int, required=True, help="The column to extract"
-    )
-    parser_extract.add_argument(
-        "-sp",
+@app.command("extract_cell")
+def extract_cell(
+    input_path: str = typer.Option(
+        ..., "--input-path", "-i", help="Path to the input folder"
+    ),
+    output_path: str = typer.Option(
+        ..., "--output-path", "-o", help="Path to the output folder"
+    ),
+    x: int = typer.Option(..., "-X", help="The row to extract"),
+    y: int = typer.Option(..., "-Y", help="The column to extract"),
+    slurm_partition: SlurmPartition = typer.Option(
+        SlurmPartition.spot,
         "--slurm-partition",
-        choices=["spot", "compute"],
-        default="spot",
-        help="Specificy the Slurm partition. By default, spot",
-    )
+        "-sp",
+        help="Specificy the Slurm partition.",
+    ),
+    p: int = typer.Option(0, help="Number of PRE RUN years to run. By default, 0"),
+    e: int = typer.Option(0, help="Number of EQUILIBRIUM years to run. By default, 0"),
+    s: int = typer.Option(0, help="Number of SPINUP years to run. By default, 0"),
+    t: int = typer.Option(0, help="Number of TRANSIENT years to run. By default, 0"),
+    n: int = typer.Option(0, help="Number of SCENARIO years to run. By default, 0"),
+    log_level: LogLevel = typer.Option(
+        LogLevel.disabled, "--log-level", "-l", help="Set the log level"
+    ),
+):
+    """Extracts a single cell and creates a batch."""
+    all_args = {
+        "input_path": input_path,
+        "output_path": output_path,
+        "X": x,
+        "Y": y,
+        "slurm_partition": slurm_partition.value,
+        "p": p,
+        "e": e,
+        "s": s,
+        "t": t,
+        "n": n,
+        "log_level": log_level.value,
+    }
+    args = type("Args", (), all_args)()
+    ExtractCellCommand(args).execute()
 
-    add_common_dvmdostem_arguments(parser_extract)
 
-    parser_extract.set_defaults(func=lambda args: ExtractCellCommand(args).execute())
+@app.command("map")
+def map_command(
+    batches: str = typer.Option(
+        ...,
+        "--batches",
+        "-b",
+        help=(
+            "Path to store the splitted batches. The given path will be concataned "
+            "with /mnt/exacloud/$USER"
+        ),
+    ),
+):
+    """Maps the given path's status."""
+    args = type("Args", (), {"batches": batches})()
+    MapCommand(args).execute()
 
-    parser_map = subparsers.add_parser("map", help="Maps the given path's status.")
 
-    add_batch_path_argument(parser_map)
-
-    parser_map.set_defaults(func=lambda args: MapCommand(args).execute())
-
-    parser_slice_input = subparsers.add_parser(
-        "slice_input",
-        help="Slices the given input data into 10 smaller folders. "
-        "To use this command, the given input has to have at least 500,000 cells.",
-    )
-
-    parser_slice_input.add_argument(
-        "-i", "--input-path", required=True, help="Path to the input folder to slice"
-    )
-    parser_slice_input.add_argument(
-        "-o",
-        "--output-path",
-        required=True,
-        help="Path for writing the sliced input dataset",
-    )
-    parser_slice_input.add_argument(
-        "-f",
-        "--force",
-        action="store_true",
-        help="Override if the given output path exists",
-    )
-    parser_slice_input.add_argument(
-        "-l",
-        "--launch-as-job",
-        action="store_true",
-        help="Never pass this flag. It will be used internally "
-        "to lauch this command as a separate job.",
-    )
-
-    parser_slice_input.set_defaults(func=lambda args: SliceInputCommand(args).execute())
-
-    args = parser.parse_args()
-
-    if hasattr(args, "func"):
-        args.func(args)
-    else:
-        parser.print_help()
+def main():
+    app()
